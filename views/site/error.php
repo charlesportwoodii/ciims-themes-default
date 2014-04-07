3c487dd278318db5ff368240cc5944b69e608eba
Charles R. Portwood II <charlesportwoodii@users.noreply.github.com>
2014-04-07 08:31:48 -0500

    Update error.php to prevent potential XSS

--- views/site/error.php
+++ views/site/error.php
@@ -1,5 +1,5 @@
 <div class="modal-container">
 	<h1><?php echo Yii::t('DefaultTheme.main', 'Error {{code}}', array('{{code}}' => $error['code'])); ?></h1>
-	<p><?php echo $error['message']; ?></p>
+	<p><?php echo CHtml::encode($error['message']); ?></p>
 </div>
 <div class="clearfix"></div>
