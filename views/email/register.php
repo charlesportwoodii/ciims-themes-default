d9a91d7a9814f86cfbd76e9ebe50bc7842b5a79f
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-03-22 18:28:35 +0000

    Updating registration email template

--- views/email/register.php
+++ views/email/register.php
@@ -1,8 +1,8 @@
-<?php echo $user->username; ?>,<br /><br />
-<?php echo Yii::t('themes.default.main', 'Thanks for registering your account at CiiMS Blog {{blog}}. To verify your account, {{clickhere}}. This action will verify your email address and allow you to interact with features only available to registered users.', array(
+<?php echo $user->displayName; ?>,<br /><br />
+<?php echo Yii::t('themes.spectre.main', 'Thanks for registering your account at CiiMS Blog {{blog}}. To verify your account, {{clickhere}}. This action will verify your email address and allow you to interact with features only available to registered users.', array(
 		'{{blog}}' => CHtml::link(Cii::getConfig('name', Yii::app()->name), Yii::app()->createAbsoluteUrl('/')),
-		'{{clickhere}}' => CHtml::link(Yii::t('themes.default.main', 'click here'), Yii::app()->createAbsoluteUrl('/activation/'.$user->id.'/'.$hash))
+		'{{clickhere}}' => CHtml::link(Yii::t('themes.spectre.main', 'click here'), Yii::app()->createAbsoluteUrl('/activation/'.$hash))
 )); ?>
 <br /><br />
-<?php echo Yii::t('themes.default.main', 'Thank you.'); ?><br /><br />
-<?php echo Yii::t('themes.default.main', 'P.S. If you did not request this email, you may safely ignore it.'); ?>
+<?php echo Yii::t('themes.spectre.main', 'Thank you.'); ?><br /><br />
+<?php echo Yii::t('themes.spectre.main', 'P.S. If you did not request this email, you may safely ignore it.'); ?>
