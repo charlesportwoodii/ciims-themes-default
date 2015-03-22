ba56c133bb7101b39059017dc37d944c05c8485f
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-03-22 20:07:34 +0000

    Fixing password reset email

--- views/email/forgot.php
+++ views/email/forgot.php
@@ -1,7 +1,7 @@
 <?php echo $user->username; ?>,<br /><br />
 <?php echo Yii::t('themes.default.main', 'You recently notified us that you forgot your password for the following blog: {{blog}}. To reset your password, {{clickhere}} and follow the instructions on the reset page. This link is valid for 15 minutes.', array(
 	'{{blog}}' => CHtml::link(Cii::getConfig('name', Yii::app()->name), Yii::app()->createAbsoluteUrl('/')),
-	'{{clickhere}}' => CHtml::link(Yii::t('themes.default.main', 'click here'), Yii::app()->createAbsoluteUrl('/forgot/' . $hash))
+	'{{clickhere}}' => CHtml::link(Yii::t('themes.default.main', 'click here'), Yii::app()->createAbsoluteUrl('/resetpassword/' . $hash))
 )); ?>
 <br /><br />
 <?php echo Yii::t('themes.default.main', 'Thank you.'); ?><br /><br />
